77f36fb11fe64edd1ad0be67eafc93c9d35435b1
Shriya Nichenametla <nhry89@gmail.com>
2026-01-08 00:10:51 -0500

    implement nav, created base for file routes and pages

---
+++ frontend/src/app/rec-card/page.js
@@ -0,0 +1,7 @@
+export default function RecCard() {
+    return (
+        <main>
+            <h1>Based on your transaction history, here are some card recommendations</h1>
+        </main>
+    )
+}
